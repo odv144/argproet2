02cfb0688883479cecf599de2d03f4331d7ed148
odv144 <omar.virili@gmail.com>
2023-11-01 07:30:59 -0300

    subiendo trabajo de unidad 1

---
+++ unidad1/script.js
@@ -0,0 +1,15 @@
+document.getElementById("tipo_seguro").addEventListener("change", function() {
+    const tipoSeguro = this.value;
+    const valorSeguro = document.getElementById("valor");
+    
+    if (tipoSeguro === "basico") {
+       valorSeguro.innerHTML='Valor basico $500'
+        // valorSeguro.value = "$500";
+    } else if (tipoSeguro === "intermedio") {
+      valorSeguro.innerHTML='Valor intermedio $1000'
+        // valorSeguro.value = "$1000";
+    } else if (tipoSeguro === "premium") {
+        // valorSeguro.value = "$1500";
+        valorSeguro.innerHTML='Valor premium $1500'
+    }
+});
